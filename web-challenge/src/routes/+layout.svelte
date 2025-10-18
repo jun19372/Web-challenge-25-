cab8ff15942a30de7522df3fbbb60d7bad622a73
jun19372 <yeonk5404@gmail.com>
2025-10-18 01:41:32 +0000

    ..

---
+++ web-challenge/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "../app.css";
+
+	let { children } = $props();
+</script>
+
+<div class="min-h-screen flex flex-col">
+	{@render children?.()}
+
+	<div class="h-24 bg-base-100 flex justify-center">
+		<a class="w-1/2 h-full" href="/">
+			<button class="w-full h-full btn btn-ghost btn-primary">Log</button>
+		</a>
+		<a class="w-1/2 h-full" href="/stats">
+			<button class="w-full h-full btn btn-ghost btn-primary"
+				>Stats</button
+			>
+		</a>
+	</div>
+</div>
---
+++ web-challenge/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script></script>
+
+<div class="flex justify-center h-screen">
+    <button class="w-450 h-20 border rounded-1px">123</button>
+
+</div>
